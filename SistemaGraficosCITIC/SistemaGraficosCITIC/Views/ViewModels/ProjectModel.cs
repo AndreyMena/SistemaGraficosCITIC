75d79bc712dea96e82efee6550e3a6eafcefa726
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2023-07-06 21:33:40 -0600

    Required en project model

--- SistemaGraficosCITIC/SistemaGraficosCITIC/Views/ViewModels/ProjectModel.cs
+++ SistemaGraficosCITIC/SistemaGraficosCITIC/Views/ViewModels/ProjectModel.cs
@@ -2,14 +2,19 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SistemaGraficosCITIC.Models.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaGraficosCITIC.Views.ViewModels
 {
     public class ProjectModel
     {
+        [Required]
         public string? Name { get; set; }
+        [Required]
         public string? Type { get; set; }
         public bool isActive { get; set; }
+
+        [Required]
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
 
