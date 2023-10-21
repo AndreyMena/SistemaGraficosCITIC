a926d33e3801ff25a3e47ed2b4d00f3310620d25
Javier Donato <javidonatoh01@gmail.com>
2023-10-20 18:35:20 -0600

    Se añadio el nombre del investigador principal al crear un proyecto

--- SistemaGraficosCITIC/SistemaGraficosCITIC/Models/Domain/Exposition.cs
+++ SistemaGraficosCITIC/SistemaGraficosCITIC/Models/Domain/Exposition.cs
@@ -2,26 +2,35 @@
 {
     public class Exposition
     {
+        public string Title { get; set; }
         public Guid Id { get; set; }
         public DateTime Date { get; set; }
         public string Location { get; set; }
         public string Context { get; set; }
-
+        public string Participants { get; set; }
+        public string Speaker { get; set; }
         public Project? Project { get; set; }
 
         public Exposition()
         {
+            Title = "";
             Id = Guid.NewGuid();
             Date = DateTime.MinValue;
             Location = "";
             Context = "";
+            Participants = "";
+            Speaker = "";
         }
-        public Exposition(DateTime date, string location, string context)
+        public Exposition(string title,DateTime date, string location, string context, 
+                            string participants, string speaker)
         {
+            Title = title;
             Id = Guid.NewGuid();
             Date = date;
             Location = location;
             Context = context;
+            Participants = participants;
+            Speaker = speaker;
         }
 
     }
