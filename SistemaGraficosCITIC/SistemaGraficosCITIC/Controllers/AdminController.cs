3627fac3ad0670cdf21fe47c1288d31ff61cb443
Isabela <isabelafrr06@hotmail.com>
2023-11-14 18:36:00 -0600

    Add views

--- SistemaGraficosCITIC/SistemaGraficosCITIC/Controllers/AdminController.cs
+++ SistemaGraficosCITIC/SistemaGraficosCITIC/Controllers/AdminController.cs
@@ -1,10 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaGraficosCITIC.Data;
+using SistemaGraficosCITIC.Repositories;
 
 namespace SistemaGraficosCITIC.Controllers
 {
-    public class AdminController : Controller
+    public class AdminController : BaseController
     {
-        public IActionResult Index()
+        public AdminController(SistemaGraficosCITICContext context, SignInManager<IdentityUser> _signInManager, UserManager<IdentityUser> _userManager, IProjectRepository _projectRepository) : base(context, _signInManager, _userManager, _projectRepository)
+        {
+        }
+
+        /// <summary>
+        /// Index GET method
+        /// </summary>
+        /// <returns>The Task of action to the view</returns>
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return _context.Publication != null ?
+                        View(await _context.Project.ToListAsync()) :
+                        Problem("Entity set 'SistemaGraficosCITICContext.Publication' is null.");
+        }
+
+        /// <summary>
+        /// Index GET method
+        /// </summary>
+        /// <returns>The Task of action to the view</returns>
+        [HttpGet]
+        public async Task<IActionResult> Graphics()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Index GET method
+        /// </summary>
+        /// <returns>The Task of action to the view</returns>
+        [HttpGet]
+        public async Task<IActionResult> Configurations()
         {
             return View();
         }
